103f4ecce9fff70112d62200f89ac65164923be4
zhenbinwu <zhenbin.wu@gmail.com>
2018-07-06 16:54:40 -0500

    Add bash setup script

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+
+if [ ! -f ./keyout.pem ]; then
+  if [ ! -f $HOME/.globus/userkey.pem ]; then
+    echo "We need userkey.pem in ~/.globus for Pre/Post firing"
+  else
+    openssl rsa -in $HOME/.globus/userkey.pem  -out keyout.pem
+  fi
+fi
+
+ssh -fN -L 8080:cmsomsapi:8080 lxplus.cern.ch
+
